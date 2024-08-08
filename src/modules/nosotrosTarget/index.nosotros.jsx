141937d09f530b748a8d297584336e3c4807c783
RauEri <eliaslautaro200905ortiz@gmail.com>
2024-08-08 18:33:56 -0300

    Modificando los estilos para lar targetas de Nosotros

--- src/modules/nosotrosTarget/index.nosotros.jsx
+++ src/modules/nosotrosTarget/index.nosotros.jsx
@@ -1,35 +1,31 @@
 import './nosotros.style.css'
+import data from './info.data.json'
 
 export default function Nosotros() {
     return (
         <div className='informacion-nosotros'>
             <div className='content-info'>
                 <div className='encabezado'>
-                    <h2>¿Quienes somos?</h2>
+                    <h2>{data['target-1'].title}</h2>
                 </div>
                 <div className='body-info'>
-                    <p>Somos una escuela con 70 años de existencia con la orientación en Electromecánica, y nos
-                        encargamos de formar a los tecnicos del futuro en Rosario de la Frontera</p>
+                    <p>{data['target-1'].body}</p>
                 </div>
             </div>
             <div className='content-info'>
                 <div className='encabezado'>
-                    <h2>¿Comó trabajamos?</h2>
+                    <h2>{data['target-2'].title}</h2>
                 </div>
                 <div className='body-info'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consectetur delectus architecto
-                        aperiam sapiente ullam, eligendi soluta. Ullam sed, accusamus error ea, dolor distinctio, vero
-                        nulla facere dicta fugit velit?</p>
+                    <p>{data['target-2'].body}</p>
                 </div>
             </div>
             <div className='content-info'>
                 <div className='encabezado'>
-                    <h2>¿Que ofrecemos?</h2>
+                    <h2>{data['target-3'].title}</h2>
                 </div>
                 <div className='body-info'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam ratione sunt ullam quisquam
-                        cum error ipsum unde, iste blanditiis incidunt similique qui obcaecati recusandae? Temporibus
-                        placeat necessitatibus in impedit et!</p>
+                    <p>{data['target-3'].body}</p>
                 </div>
             </div>
         </div>
