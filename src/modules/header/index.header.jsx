5d2901c452a39f3eed03dba1dee3c56697920d52
RauEri <eliaslautaro200905ortiz@gmail.com>
2024-08-10 17:52:12 -0300

    Pequeñas modificaciones

--- src/modules/header/index.header.jsx
+++ src/modules/header/index.header.jsx
@@ -5,12 +5,10 @@ export default function Header() {
     return (
         <header id='header'>
             <nav className="main-navigation">
-                {/* <a href='banner-conteiner'> */}
                     <div className='site-logo'>
                         <img src={logo} className='logo' alt='Logo' />
                         <h3>EET N°3132</h3>
                     </div>
-                {/* </a> */}
                 <div className='main-menu'>
                     <ul className='main-list'>
                         <li><a href="#banner-conteiner" className="nav-link">Inicio</a></li>
